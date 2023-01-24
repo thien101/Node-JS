d08176405e79c98abde41081ab5c2e00228bd455
thien101 <tkgiongmk010@gmail.com>
2023-01-24 11:54:16 +0700

    scss & static file

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const { engine } = require ('express-handlebars');
+
+const morgan = require('morgan');
+const { extname } = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('combined'))
+// Template engine
+app.engine('hbs', engine({
+    extname: '.hbs',
+  }
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})  
+
+app.get('/infor', (req, res) => {
+  res.render('infor')
+}) 
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
