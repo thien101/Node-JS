332c7916d097174d31c95bb112b755370718013f
thien101 <tkgiongmk010@gmail.com>
2023-01-27 17:30:14 +0700

    install prettier husky list staged

--- src/main.js
+++ src/main.js
@@ -19,21 +19,18 @@ app.use(express.json());
 
 //app.use(morgan('combined'))
 // Template engine
-  app.engine(
-      'hbs',
-      engine({
-          extname: '.hbs',
-      }),
-  );
-app.set('view engine', 
-'hbs');
-app.set('views', path.join(__dirname, 
-  'resources\\views'));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
 
 // Routes init: khởi tạo tuyến đường
-route(app);
+route(      app);
 
-app.listen(port, () => {
-    console.log(`Example app liste
-    ning on port ${port}`);
-});
+          app.listen(port, () => {
+              console.log(`Example app listening on port ${port}`);
+          });
