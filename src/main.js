da99ddf4ddab00f2e79b869ef164b7d406eb8afd
thien101 <tkgiongmk010@gmail.com>
2023-01-27 17:51:30 +0700

    test husky5

--- src/main.js
+++ src/main.js
@@ -31,6 +31,6 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 // Routes init: khởi tạo tuyến đường
 route(app);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
