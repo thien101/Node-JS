03799dd9aba674a85f7f109321407309b4870225
thien101 <tkgiongmk010@gmail.com>
2023-01-27 17:44:10 +0700

    install prettier husky list staged2

--- src/main.js
+++ src/main.js
@@ -29,8 +29,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
 // Routes init: khởi tạo tuyến đường
-route(      app);
+        route(app);
 
-          app.listen(port, () => {
-              console.log(`Example app listening on port ${port}`);
-          });
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
